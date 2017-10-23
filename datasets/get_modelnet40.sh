a7f8f048ae4d726a5eee44e305f847586dcf819c
Nima Sedaghat <n.sedaghat@gmail.com>
2017-10-23 18:35:51 +0200

    fixed modelnet40 download link

--- datasets/get_modelnet40.sh
+++ datasets/get_modelnet40.sh
@@ -6,7 +6,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 
 #--- Download the compressed file
-wget -nc http://vision.princeton.edu/projects/2014/3DShapeNets/ModelNet40.zip
+wget -nc http://modelnet.cs.princeton.edu/ModelNet40.zip
 
 #--- Extract it
 unzip ModelNet40
