774de1608829a0e72da3446deb3c6d03bd6d6849
Nima Sedaghat <n.sedaghat@gmail.com>
2017-11-03 17:02:33 +0100

    download the original sydney dataset, to use folds lists

--- datasets/get_sydney.sh
+++ datasets/get_sydney.sh
@@ -5,11 +5,13 @@ backpath=`pwd`
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 
-#--- Download the compressed file -- this is our aligned version
-wget -nc https://lmb.informatik.uni-freiburg.de/resources/datasets/ORION/aligned_sydney_objects.tar.gz
+#--- Download the compressed file
+wget -nc http://www.acfr.usyd.edu.au/papers/data/sydney-urban-objects-dataset.tar.gz #This is the original dataset (we just need some lists from it)
+wget -nc https://lmb.informatik.uni-freiburg.de/resources/datasets/ORION/aligned_sydney_objects.tar.gz # This is our version, containing PLY files, aligned and original
 
 #--- Extract it
-mkdir -p Sydney
+mkdir -p Sydney/original
+tar -xvf sydney-urban-objects-dataset.tar.gz -C Sydney/original
 tar -xvf aligned_sydney_objects.tar.gz -C Sydney
 
 
